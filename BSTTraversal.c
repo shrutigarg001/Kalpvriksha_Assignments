32004f58592408772d87e1802abb3e983d7753b7
shrutigarg001 <96514532+shrutigarg001@users.noreply.github.com>
2023-04-09 23:14:13 +0530

    Add files via upload

---
+++ BSTTraversal.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int value;
+    struct node *left, *right;
+};
+
+struct node* newNode(int node)
+{
+    struct node* temp
+        = (struct node*)malloc(sizeof(struct node));
+    temp->value = node;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+void inorderPrint(struct node* root)
+{
+    if (root != NULL) {
+        inorderPrint(root->left);
+        printf("%d ", root->value);
+        inorderPrint(root->right);
+    }
+} 
+
+void preorderPrint(struct node* root)
+{
+    if (root != NULL) {
+        printf("%d ", root->value); 
+        preorderPrint(root->left); 
+        preorderPrint(root->right);
+    }
+} 
+
+void postorderPrint(struct node* root)
+{
+    if (root != NULL) {
+        postorderPrint(root->left);
+        postorderPrint(root->right); 
+        printf("%d ", root->value); 
+
+    }
+} 
+
+struct node* insert(struct node* node, int value)
+{
+    if (node == NULL)
+        return newNode(value);
+
+    if (value < node->value)
+        node->left = insert(node->left, value);
+    else if (value > node->value)
+        node->right = insert(node->right, value);
+
+    return node;
+}
+
+int main()
+{
+    struct node* root = NULL;
+    root = insert(root, 50);
+    insert(root, 10);
+    insert(root, 20);
+    insert(root, 30);
+    insert(root, 40);
+    insert(root, 50);
+    insert(root, 60);
+
+    inorderPrint(root);
+
+    return 0;
+} 
